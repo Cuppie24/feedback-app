3c48830fcacd0dc27c87f286badb8719613228cd
Cuppie <ishnazarovsaidnazar24@gmail.com>
2026-09-16 23:43:37 +0500

    fixed double scrollbar on error ticket view in user mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { FeedbackApp } from './features/feedback/components/FeedbackApp.tsx'
+import App from './App.tsx'
+import { AuthProvider } from './context/AuthContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <FeedbackApp />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
